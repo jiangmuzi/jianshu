507384a63b3a852883f44a7acd0bf3571b9e98e0
jiangmuzi <sisome@qq.com>
2015-10-13 15:38:25 +0800

    update theme

--- index.php
+++ index.php
@@ -19,10 +19,11 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 		  <?php showThumb($this);?>
 		<?php endif; ?>
 		<ul class="post-meta">
-		    <li><?php _e('<i class="fa fa-book"></i> '); ?><?php $this->category(','); ?></li>
-		    <li><?php _e('<i class="fa fa-clock-o"></i> '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php echo formatTime($this->created); ?></time></li>
-		      	<li><?php _e('<i class="fa fa-eye"></i> 阅读 '); ?><?php $this->viewsNum(); ?></li>
-			<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#<?php $this->respondId(); ?>"><?php $this->commentsNum('<i class="fa fa-comments-o"></i> 评论 %d'); ?></a></li>
+		    <li><?php $this->category(','); ?></li>
+		    <li><?php $this->dateWord(); ?></li>
+			<li><?php _e('阅读 '); ?><?php $this->viewsNum(); ?></li>
+			<li><?php _e('喜欢 '); ?><?php $this->likesNum(); ?></li>
+			<li href="<?php $this->permalink() ?>#<?php $this->respondId(); ?>"><?php $this->commentsNum('评论 %d'); ?></a></li>
 		</ul>
 		<h2 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
         <?php if(!empty($this->options->listStyle) && in_array('excerpt',$this->options->listStyle)): ?>
