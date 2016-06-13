0ac1d5a697bccee040876da597b0fbb1dc2ae6f9
jiangmuzi <sisome@qq.com>
2016-06-13 20:59:03 +0800

    Update header.php
    fix bug

--- themes/jianshu/header.php
+++ themes/jianshu/header.php
@@ -99,7 +99,7 @@ if(!function_exists('getReadMode')) include_once('functions.php');
 			<?php endif; ?>
 		</li>
 		<?php endif; ?>
-		<?php if($this->is('archive') && !empty($this->getArchiveTitle())):?>
+		<?php if($this->is('archive')):?>
 		<li class="active">
 			<a href="javascript:jBlog.goTop();"><?php _e($this->getArchiveTitle()); ?></a>
 		</li>
